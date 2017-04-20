52e1547b17954ef732a71272ced4d4da631b42df
Jim Kang <jimkang@gmail.com>
2017-04-19 22:08:36 -0400

    Rewrote updateIndexHTMLInGit test.

---
+++ tests/transforms/update-index-html-in-git-tests.js
@@ -0,0 +1,87 @@
+var test = require('tape');
+var StreamTestBed = require('through-stream-testbed');
+var UpdateIndexHTMLInGit = require('../../transforms/update-index-html-in-git');
+var config = require('../../config');
+var request = require('request');
+var randomId = require('idmaker').randomId;
+var probable = require('probable');
+var range = require('d3-array').range;
+
+var numberOfCells = probable.rollDie(6);
+
+var cells = [
+  {
+    newLastPageIndex: 0,
+    updatedPages: [
+      {
+        index: 0,
+        cells: [
+          {
+            "htmlFragment": "<li class=\"video-pane\">\n  <div class=\"video-time-stamp video-meta\"><time datetime=\"2017-04-18T13:34:01.000Z\"</div>\n  <video controls loop=\"true\" preload=\"metadata\" src=\"../lookit/videos/34U_5EzYg4Bvy88n.mp4\"></video>\n  <div class=\"video-caption video-meta\"></div>\n</li>"
+          },
+          {
+            "htmlFragment": "<li class=\"video-pane\">\n  <div class=\"video-time-stamp video-meta\"><time datetime=\"2017-04-18T13:42:24.000Z\"</div>\n  <video controls loop=\"true\" preload=\"metadata\" src=\"../lookit/videos/QBytlk6nvVUhhrT1.mp4\"></video>\n  <div class=\"video-caption video-meta\">Short protestor</div>\n</li>"
+          }
+        ]
+      }
+    ]
+  },
+
+  {
+    newLastPageIndex: 1,
+    updatedPages: [
+      {
+        index: 1,
+        cells: [
+          {
+            "htmlFragment": "<li class=\"video-pane\">\n  <div class=\"video-time-stamp video-meta\"><time datetime=\"2017-04-18T21:33:38.000Z\"</div>\n  <video controls loop=\"true\" preload=\"metadata\" src=\"../lookit/videos/ik17VrjkklDF-Q19.mp4\"></video>\n  <div class=\"video-caption video-meta\">Christmas in April</div>\n</li>"
+          }
+        ]
+      },
+      {
+        index: 0,      
+        cells: [
+          {
+            "htmlFragment": "<li class=\"video-pane\">\n  <div class=\"video-time-stamp video-meta\"><time datetime=\"2017-04-18T13:26:20.000Z\"</div>\n  <video controls loop=\"true\" preload=\"metadata\" src=\"../lookit/videos/undefined\"></video>\n  <div class=\"video-caption video-meta\">Not how mashups are made, guy.</div>\n</li>"
+          },
+          {
+            "htmlFragment": "<li class=\"video-pane\">\n  <div class=\"video-time-stamp video-meta\"><time datetime=\"2017-04-18T13:34:01.000Z\"</div>\n  <video controls loop=\"true\" preload=\"metadata\" src=\"../lookit/videos/34U_5EzYg4Bvy88n.mp4\"></video>\n  <div class=\"video-caption video-meta\"></div>\n</li>"
+          },
+          {
+            "htmlFragment": "<li class=\"video-pane\">\n  <div class=\"video-time-stamp video-meta\"><time datetime=\"2017-04-18T13:42:24.000Z\"</div>\n  <video controls loop=\"true\" preload=\"metadata\" src=\"../lookit/videos/QBytlk6nvVUhhrT1.mp4\"></video>\n  <div class=\"video-caption video-meta\">Short protestor</div>\n</li>"
+          }
+        ]
+      }
+    ]
+  },
+];
+
+var updateIndexHTMLInGit = UpdateIndexHTMLInGit({
+  branch: 'master',
+  gitRepoOwner: config.githubTest.gitRepoOwner,
+  gitToken: config.githubTest.gitToken,
+  repo: config.githubTest.repo,
+  request: request,
+  shouldSetUserAgent: true,
+  metaDir: 'video/meta'
+});
+
+test(
+  'Test creating index pages for ' + numberOfCells + ' cells to index in git',
+  StreamTestBed({
+    transformFn: updateIndexHTMLInGit,
+    inputItems: cells,
+    checkCollectedStreamOutput: checkGitResults,
+    checkOutputItem: checkGitResult
+  })
+);
+
+function checkGitResults(t, resultCells) {
+  t.equal(resultCells.length, cells.length, 'There is a git result object for each cell.');
+  console.log('Look at the repo to verify the correct updates were committed.');
+}
+
+function checkGitResult(t, resultCell) {
+  t.ok(resultCell.indexHTMLs.length > 0, 'There is at least one index html.');
+  console.log(resultCell.indexHTMLs);
+}
